767c7b35964982a0ba188c855078d70c521fba78
Áriston <aaa_10@cin.ufpe.br>
2025-01-28 09:39:06 -0300

    Esqueleto da integração com a API pronto

---
+++ Back-end/api_contact.py
@@ -0,0 +1,85 @@
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+from IPython.display import Image
+from IPython.core.display import HTML
+
+dependencies = [
+    "grpcio==1.67.1"  # Se der timeout, reduzir o grpcio para essa versão
+]
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+GOOGLE_API_KEY='AIzaSyB6Up-EDbZa3wwT-ti0cWDnGzh3GRqQELc'
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+img = Image('Back-end/assets/bg1.png')
+img2 = Image('Back-end/assets/Mico_azul1.jpeg')
+
+Validade_1 = model.generate_content(["""Avalie o conteúdo fornecido e determine se ele possui informações suficientes e estruturadas para 
+                                     criar perguntas significativas. Por exemplo, rejeite conteúdos muito curtos (menos de 10 palavras úteis) 
+                                     ou formados apenas por frases desconexas.Responda apenas com True ou False. Retorne True caso ele seja 
+                                     coerente caso contrário retorne False""", img], stream=True)
+Validade_1.resolve()
+print(Validade_1.text)
+
+Validade_2 = model.generate_content(["""Determine se o conteúdo fornecido aborda um tema ou assunto definido. Responda apenas com True ou False. 
+                                     Caso o texto ou imagem seja completamente genérico ou irrelevante, Retorne False.""", img], stream=True)
+Validade_2.resolve()
+print(Validade_2.text)
+
+Validade_3 = model.generate_content(["""Verifique se o conteúdo fornecido é lógico, coerente e compreensível.Responda apenas com True ou False. 
+                                     Se ele contiver apenas elementos desorganizados, sem conexão ou significado claro, Retorne False, caso 
+                                     contrário retorne True.""", img], stream=True)
+Validade_3.resolve()
+print(Validade_3.text)
+
+valid_response = False
+
+if "True" in Validade_1.text and "True" in Validade_3.text and  "True" in Validade_3.text:
+
+    response = model.generate_content([
+        """Analise o conteúdo do documento ou imagem fornecido e, com base nas informações apresentadas, elabore perguntas relevantes relacionadas ao tema principal. Certifique-se de que cada pergunta possa ser respondida exclusivamente com as informações presentes no conteúdo, sem depender de conhecimento externo.
+            Para cada pergunta:
+
+            Forneça quatro opções de resposta (A, B, C, D) que sejam plausíveis e estejam claramente conectadas ao conteúdo.
+            Destaque qual é a resposta correta entre as opções.
+            Valide se a pergunta pode ser respondida diretamente com as informações fornecidas.
+            Apresente o resultado no formato JSON estruturado conforme o modelo abaixo:
+
+            {
+            "questions": [
+                {
+                "question": "Texto da pergunta.",
+                "options": {
+                    "A": "Opção A",
+                    "B": "Opção B",
+                    "C": "Opção C",
+                    "D": "Opção D"
+                },
+                "correct_answer": "Letra da resposta correta (A, B, C ou D)",
+                "validation": "Explicação de como a pergunta pode ser respondida com base no conteúdo fornecido."
+                }
+                // Repetir estrutura para cada pergunta gerada.
+            ],
+            "validation_status": "ok" // Ou "insufficient_content" caso o conteúdo não permita a criação de perguntas relevantes.
+            }
+""", img], stream=True)
+    response.resolve()
+    valid_response = True
+else:
+    print("Documento fornecido não possui conteudo claro o suficiente para gerar perguntas")
+    valid_response = False
+
+if valid_response == True:
+    print(response.text)
--- front-end/StudyWise/components/ui/index-background.tsx
+++ front-end/StudyWise/components/ui/index-background.tsx
@@ -5,12 +5,12 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function IndexBackground({children, ...rest}: ViewProps) {
     return (
-        <SafeAreaView style={styles.container}>
-                <LinearGradient style = {styles.container} colors = {['#0054C1', '#00B7C9']}>
-                    <StatusBar backgroundColor={'#0054C1'}/>
-                    {children}
-                </LinearGradient>
-        </SafeAreaView>
+        
+        <LinearGradient style = {styles.container} colors = {['#0054C1', '#00B7C9']}>
+            <StatusBar backgroundColor={'#0054C1'}/>
+            {children}
+        </LinearGradient>
+        
     )
 }
 
