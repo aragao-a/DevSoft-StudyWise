625a86cf7a61f4381601383951957f2a6b8c319e
Marcus Vinicius <mvinne0084@hotmail.com>
2025-01-29 00:17:55 -0300

    Update

--- Back-end/api_contact.py
+++ Back-end/api_contact.py
@@ -20,7 +20,7 @@ def to_markdown(text):
   text = text.replace('•', '  *')
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-GOOGLE_API_KEY='AIzaSyBE3WdBNhe415m9vPTrjAPPEjqXlk3PAqc'
+GOOGLE_API_KEY=''
 
 genai.configure(api_key=GOOGLE_API_KEY)
 
