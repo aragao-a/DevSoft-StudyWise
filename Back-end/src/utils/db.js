13e9204f67216df0598a97165a5eba6984845472
Kelvin Carvalho <kelvin.carvalho@wesafer.com>
2025-03-15 03:19:54 -0300

    add automated tests to server routes

--- Back-end/src/utils/db.js
+++ Back-end/src/utils/db.js
@@ -1,9 +1,12 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
+import * as dotenv from "dotenv";
+dotenv.config();
+const DB_PATH = process.env.DB_PATH;
 
 // Abre a conexão com o banco de dados SQLite
 const db = await open({
-  filename: "./database.db", // Caminho para o arquivo do banco de dados
+  filename: DB_PATH, // Caminho para o arquivo do banco de dados
   driver: sqlite3.Database,
 });
 
